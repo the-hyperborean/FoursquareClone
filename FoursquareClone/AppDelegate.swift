e0bf76fea98a4d6f83e011f5dc9cbff060ee90a3
the-hyperborean <jtb@abaris.com.au>
2020-12-26 16:23:31 +1100

    Completed Parse Setup

--- FoursquareClone/AppDelegate.swift
+++ FoursquareClone/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let configuration = ParseClientConfiguration { (ParseMutableClientConfiguration) in
+            ParseMutableClientConfiguration.applicationId = "PbMS8T6u33GtLVi6OZPMBeukqgrBZVnA0hoa0oQJ"
+            ParseMutableClientConfiguration.clientKey = "m2xddzuBHI5zD3GP4bxZJAuX9xWSAnZYodaT6bo6"
+            ParseMutableClientConfiguration.server = "https://parseapi.back4app.com/"
+        }
+        
+        Parse.initialize(with: configuration)
+        
         return true
     }
 
