09f54f3770d95eb2d8537fb29566fa382b53e399
the-hyperborean <jtb@abaris.com.au>
2020-12-26 16:38:07 +1100

    Test write to Parse DB in viewDidLoad

--- FoursquareClone/ViewController.swift
+++ FoursquareClone/ViewController.swift
@@ -13,7 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        let parseObject = PFObject(className: "Fruits")
+        parseObject["name"] = "Apples"
+        parseObject["calories"] = 100
+        parseObject.saveInBackground { (success, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                print("uploaded")
+            }
+        }
     }
+    
+    
 
 
 }
