2f9509f3f81faf895ebb5dfadac278829b7acea5
the-hyperborean <jtb@abaris.com.au>
2020-12-28 15:24:39 +1100

    Added Logout Functionality

--- FoursquareClone/PlacesVC.swift
+++ FoursquareClone/PlacesVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class PlacesVC: UIViewController {
 
@@ -15,9 +16,25 @@ class PlacesVC: UIViewController {
         super.viewDidLoad()
 
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logoutButtonClicked))
     }
     
     @objc func addButtonClicked() {
+        
+    }
+    
+    @objc func logoutButtonClicked() {
+        
+        PFUser.logOutInBackground { (error) in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                self.performSegue(withIdentifier: "toSignUpVC", sender: nil)
+            }
+        }
         
     }
     
