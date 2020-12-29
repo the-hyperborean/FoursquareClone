3cafa60a7cfdfa2de59da20ca1dbf33092d09740
the-hyperborean <jtb@abaris.com.au>
2020-12-29 15:30:10 +1100

    Photo Picker Completed

--- FoursquareClone/AddPlaceVC.swift
+++ FoursquareClone/AddPlaceVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AddPlaceVC: UIViewController {
+class AddPlaceVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var placeNameText: UITextField!
     @IBOutlet weak var placeTypeText: UITextField!
@@ -18,10 +18,26 @@ class AddPlaceVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        placeImageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        placeImageView.addGestureRecognizer(gestureRecognizer)
     }
 
     @IBAction func nextButtonClicked(_ sender: Any) {
         performSegue(withIdentifier: "toMapVC", sender: nil)
+    }
+    
+    @objc func chooseImage() {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        placeImageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
 }
